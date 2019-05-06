d3162d8963c72f799fb080d3451f20c896e9fba1
Jason Laster <jlaster@mozilla.com>
2019-05-06 09:39:30 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import results from "./results"
 
 function App() {
+  const table = results.splice(0, 20)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Results
+{table.map(r => <div>{r[0]}</div>)}
       </header>
+
     </div>
   );
 }
